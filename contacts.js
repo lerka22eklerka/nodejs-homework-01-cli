1ac5d9acedae9702d54d38f029ec7245df0ccb80
lerka22eklerka <94803605+lerka22eklerka@users.noreply.github.com>
2022-11-30 13:26:36 +0200

    done

---
+++ contacts.js
@@ -0,0 +1,63 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const contactsPath = path.resolve("./db/contacts.json");
+
+async function listContacts() {
+  try {
+    const data = await fs.readFile(contactsPath, "utf8");
+    return JSON.parse(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function getContactById(contactId) {
+  try {
+    const contacts = await listContacts();
+    const result = contacts.find((contact) => contact.id === contactId);
+    if (!result) {
+      return null;
+    }
+    return result;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function removeContact(contactId) {
+  try {
+    const contacts = await listContacts();
+    const index = contacts.findIndex(
+      (contact) => contact.id === contactId);
+    if (index === -1) {
+      return null;
+    }
+    const removedContact = contacts.splice(index, 1);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    //  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+    return removedContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+async function addContact(name, email, phone) {
+  try {
+      const contacts = await listContacts();
+      const id = contacts.length + 1;
+    const newContact = { id, name, email, phone };
+    contacts.push(newContact);
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return newContact;
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
